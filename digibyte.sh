167196b7d85fcefdad305d811a94b60dc79b0203
cryptocodeman <94156532+cryptocodeman@users.noreply.github.com>
2021-11-11 22:19:50 -0600

    Update digibyte.sh

--- digibyte.sh
+++ digibyte.sh
@@ -4,12 +4,14 @@ mkdir ~/.viacoin/
 mkdir ~/.artiqox/
 mkdir ~/.argentum/
 mkdir ~/.myriadcoin/
+mkdir ~/.dingocoin
 sudo cp -i digibyte.conf ~/.digibyte/
 sudo cp -i dogecoin.conf ~/.dogecoin/
 sudo cp -i viacoin.conf ~/.viacoin/
 sudo cp -i artiqox.conf ~/.artiqox/
 sudo cp -i argentum.conf ~/.argentum/
 sudo cp -i myriadcoin.conf ~/.myriadcoin/
+sudo cp -i dingocoin.conf ~/.dingocoin/
 cd ~/p2pool-oneclick/Start-P2pool
 chmod a+x dgbp2poolzen.desktop
 cd ..
